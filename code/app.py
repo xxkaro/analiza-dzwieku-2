611459cd7b926436096ce48b2bb2ad025cb7b1e7
“xxkaro” <dunal.karolina@gmail.com>
2025-04-21 21:56:33 +0200

    part for grade 4 done

--- code/app.py
+++ code/app.py
@@ -31,10 +31,12 @@ uploaded_file = st.file_uploader("Wybierz plik WAV", type=["wav"])
 clip_duration = None
 frame_size = None
 hop_length = None  
+f, a = None, None
 
 if uploaded_file is not None:
     st.audio(uploaded_file)
     y, sr = librosa.load(uploaded_file, sr=None)
+    y = y - np.mean(y)
 
     col1, col2 = st.columns([1, 3]) 
 
@@ -47,10 +49,13 @@ if uploaded_file is not None:
         frame_duration = int(frame_duration[:-2])  
         frame_size = int(frame_duration * sr / 1000)
 
+        hop_length = st.slider("Wybierz długość przesunięcia ramek (overlap):", 1, frame_size, frame_size // 2)
+        hop_length = int(hop_length)
+
         if analysis_type == "Konkretna ramka":
             start_time_ms = st.text_input("Czas startu ramki (ms):", "0")
             start_time_ms = int(start_time_ms)
-            end_time_ms = st.text_input("Czas końca ramki (ms):", "0")
+            end_time_ms = st.text_input("Czas końca ramki (ms):", "1000")
             end_time_ms = int(end_time_ms)
 
             if end_time_ms > len(y):
@@ -61,17 +66,39 @@ if uploaded_file is not None:
                 st.warning("Czas startu ramki musi być większy od 0!")
             else:
                 y = y[int(start_time_ms * sr / 1000): int(end_time_ms * sr / 1000)]
-        # else:
-        #     y, sr = librosa.load(uploaded_file, sr=None)
+       
+        window_type = st.radio(
+        "Wybierz funkcję okienkową:",
+        ["Prostokątne", "Trójkątne", "Hamming", "Hann", "Blackman"],
+        index=0 
+        )
+        
+        if window_type == "Prostokątne":
+            window_type = 'rectangular'
+        elif window_type == "Trójkątne":
+            window_type = 'triangular'
+        elif window_type == "Hamming":
+            window_type = 'hamming'
+        elif window_type == "Hann":
+            window_type = 'hanning'
+        elif window_type == "Blackman":
+            window_type = 'blackman'
 
 
     with col2:
         st.write("### Wykres przebiegu czasowego audio:")
+        y = apply_window_to_frames(y, frame_size, hop_length, window_type)
+        frame_features = extract_frame_features(y, sr, frame_size, hop_length)  
+        num_frames = len(frame_features['volume'])
+
         if analysis_type == "Cały sygnał":
             time_axis = np.linspace(0, len(y) / sr, len(y))
+            frame_time_axis = np.linspace(0, len(y) / sr, num=num_frames)
 
         if analysis_type == "Konkretna ramka":
             time_axis = np.linspace(start_time_ms / 1000, end_time_ms / 1000, len(y))
+            frame_time_axis = np.linspace(start_time_ms / 1000, end_time_ms / 1000, num=num_frames)
+
 
         fig = go.Figure()
 
@@ -83,19 +110,30 @@ if uploaded_file is not None:
                                             "<b>Amplituda:</b> %{y:.3f}" + "<extra></extra>", 
         ))
         fig.update_layout(
-            title="Przebieg czasowy",
-            title_x=0.5,
             xaxis_title="Czas (s)",
             yaxis_title="Amplituda",
             hovermode="closest"  
         )
 
         st.plotly_chart(fig)
-        
-    
+
+
+    col1, col2 = st.columns(2) 
+
+
+    with col1:
+        st.write("### Spektrogram:")
+        spectrogram = compute_spectrogram(y, frame_size, hop_length, window_type)
+        fig = plot_spectrogram(spectrogram, sr, frame_size, hop_length)
+        st.plotly_chart(fig, use_container_width=True)
+
+    with col2:
+        st.write("### Wykres widma częstotliwościowego:")
+        f, a = frequency_spectrum(y, sr)
+        st.plotly_chart(plot_spectrum(f,a), use_container_width=True)
+
+
     st.write("### Analiza w dziedzinie częstotliwości:")
-    hop_length = frame_size // 2
-    frame_features = extract_frame_features(y, sr, frame_size, hop_length)  
 
     features = [
         ('volume', frame_features['volume']),
@@ -107,24 +145,25 @@ if uploaded_file is not None:
     ]
 
     num_features = len(features)
-    
+ 
     col1, col2 = st.columns(2)
     with col1:
-        st.plotly_chart(plot_feature(time_axis, features[0][1], features[0][0], "ramki"), use_container_width=True)
+        st.plotly_chart(plot_feature(frame_time_axis, features[0][1], features[0][0], "ramki"), use_container_width=True)
     with col2:
-        st.plotly_chart(plot_feature(time_axis, features[1][1], features[1][0], "ramki"), use_container_width=True)
+        st.plotly_chart(plot_feature(frame_time_axis, features[1][1], features[1][0], "ramki"), use_container_width=True)
 
     col3, col4 = st.columns(2)
     with col3:
-        st.plotly_chart(plot_feature(time_axis, features[2][1], features[2][0], "ramki"), use_container_width=True)
+        st.plotly_chart(plot_feature(frame_time_axis, features[2][1], features[2][0], "ramki"), use_container_width=True)
     with col4:
-        st.plotly_chart(plot_feature(time_axis, features[5][1], features[5][0], "ramki"), use_container_width=True)
+        st.plotly_chart(plot_feature(frame_time_axis, features[5][1], features[5][0], "ramki"), use_container_width=True)
 
     col5, col6 = st.columns(2)
     with col5:  
-        st.plotly_chart(plot_feature(time_axis, features[4][1], features[4][0], "ramki"), use_container_width=True)
+        st.plotly_chart(plot_feature(frame_time_axis, features[4][1], features[4][0], "ramki"), use_container_width=True)
     with col6:  
-        st.plotly_chart(plot_ersb(time_axis, features[3][1], "ramki"), use_container_width=True)
+        st.plotly_chart(plot_ersb(frame_time_axis, features[3][1], "ramki", sr), use_container_width=True)
+    
 
 
 
--- code/audio_analysis.py
+++ code/audio_analysis.py
@@ -4,7 +4,7 @@ import plotly.graph_objects as go
 import librosa
 
 
-def frequency_spectrum(y, sr):
+def frequency_spectrum(y, sr, window_type=None):
     N = len(y)
     y_fft = fft(y)
     freqs = fftfreq(N, 1/sr)
@@ -14,26 +14,40 @@ def frequency_spectrum(y, sr):
     amplitude_list = np.abs(y_fft[idx]).tolist()
     return freqs_list, amplitude_list
 
-def apply_window(y, sr, window_type='hamming', frame_size=1024):
+def apply_window(frame, window_type):
     if window_type == 'rectangular':
-        window = np.ones(frame_size)
+        window = np.ones(len(frame))  # Okno prostokątne
     elif window_type == 'triangular':
-        window = np.bartlett(frame_size)
+        window = np.bartlett(len(frame))  # Okno trójkątne
     elif window_type == 'hamming':
-        window = np.hamming(frame_size)
-    elif window_type == 'hann':
-        window = np.hanning(frame_size)
+        window = np.hamming(len(frame))  # Okno Hamming'a
+    elif window_type == 'hanning':
+        window = np.hanning(len(frame))  # Okno Hanninga
+    elif window_type == 'blackman':
+        window = np.blackman(len(frame))  # Okno Blackmana
     else:
-        raise ValueError("Unknown window type")
+        raise ValueError(f"Nieznany typ okna: {window_type}")
+    
+    return frame * window
 
-    frame = y[:frame_size] * window
-    t_frame = np.arange(frame_size) / sr
-    t_list = t_frame.tolist()
-    frame_list = frame.tolist()
+def apply_window_to_frames(y, frame_size, hop_length, window_type='rectangular'):
+    y_windowed = np.zeros(len(y))
 
-    freqs_list, amplitude_list = frequency_spectrum(frame, sr)
+    num_frames = (len(y) - frame_size) // hop_length + 1
+    
+    for i in range(num_frames):
+        start = i * hop_length
+        end = start + frame_size
+        
+        frame = y[start:end]
+        
+        windowed_frame = apply_window(frame, window_type)
+        
+        y_windowed[start:end] += windowed_frame
 
-    return t_list, frame_list, freqs_list, amplitude_list
+    return y_windowed
+
+
 
 def generate_test_y(frequency=1000, sr=10000, duration=1):
     t = np.linspace(0, duration, int(sr * duration), endpoint=False)
@@ -56,15 +70,22 @@ def compute_band_energy(frequencies, spectrum, f0, f1):
     band_mask = (frequencies >= f0) & (frequencies < f1)
     return np.sum(np.square(np.abs(spectrum[band_mask])))
 
-def compute_band_energy_ratios(frequencies, spectrum):
+def compute_band_energy_ratios(sr, frequencies, spectrum):
     total_energy = np.sum(np.square(np.abs(spectrum)))
-    subbands = [(0, 630), (630, 1720), (1720, 4400), (4400, 11025)]
+    # subbands = [(0, 630), (630, 1720), (1720, 4400), (4400, 11025)]
+    f_max = sr / 2
+    subbands = [
+        (0, int(0.057 * f_max)),
+        (int(0.057 * f_max), int(0.156 * f_max)),
+        (int(0.156 * f_max), int(0.399 * f_max)),
+        (int(0.399 * f_max), int(f_max))
+    ]
     ratios = []
-    for f0, f1 in subbands[:-1]:  # tylko 3 pierwsze, jak w opisie
+    for f0, f1 in subbands: 
         band_energy = compute_band_energy(frequencies, spectrum, f0, f1)
         ratio = band_energy / total_energy if total_energy > 0 else 0
         ratios.append(ratio)
-    return ratios  # [ERSB1, ERSB2, ERSB3]
+    return ratios
 
 def get_band_indices(frequencies, f_low=100, f_high=6000):
     f_low_idx = np.argmin(np.abs(frequencies - f_low))
@@ -89,9 +110,7 @@ def compute_spectral_crest(spectrum, frequencies, f_low=100, f_high=6000):
     return np.max(power) / np.mean(power) if np.mean(power) > 0 else 0
 
 
-def extract_frame_features(y, sr, frame_size_ms, hop_length):
-    frame_size = int(sr * frame_size_ms / 1000)
-
+def extract_frame_features(y, sr, frame_size, hop_length):
     frame_features = {
         'volume': [],
         'centroid': [],
@@ -103,13 +122,19 @@ def extract_frame_features(y, sr, frame_size_ms, hop_length):
 
     for start in range(0, len(y) - frame_size + 1, hop_length):
         frame = y[start:start + frame_size]
-        frequencies = get_frequencies(len(frame), sr)
-        spectrum = fft(frame)[:frame_size // 2]
+    
+        # Sprawdzamy, czy mamy ostatnią ramkę (może być krótsza)
+        if start + frame_size > len(y):
+            frame = y[start:]  # pobieramy pozostałą część sygnału
 
+        frequencies = get_frequencies(len(frame), sr)
+        spectrum = fft(frame)[:len(frame) // 2]
+
+        # Obliczamy cechy dla tej ramki
         volume = compute_volume(spectrum)
         centroid = compute_frequency_centroid(frequencies, spectrum)
         bandwidth = compute_bandwidth(frequencies, spectrum)
-        energy_ratios = compute_band_energy_ratios(frequencies, spectrum)
+        energy_ratios = compute_band_energy_ratios(sr, frequencies, spectrum)
         flatness = compute_spectral_flatness(spectrum, frequencies)
         crest = compute_spectral_crest(spectrum, frequencies)
 
@@ -122,15 +147,54 @@ def extract_frame_features(y, sr, frame_size_ms, hop_length):
 
     return frame_features
 
+
 def get_frequencies(N, sr):
     return np.fft.fftfreq(N, d=1/sr)[:N // 2]
 
+
+def compute_spectrogram(y, frame_size, hop_length, window='rectangular'):
+
+    n_frames = (len(y) - frame_size) // hop_length + 1
+    spectrogram = np.zeros((n_frames, frame_size // 2 + 1)) 
+
+    for i in range(n_frames):
+        start = i * hop_length
+        frame = y[start:start + frame_size]
+        frame = apply_window(frame, window)
+        spectrum = np.abs(np.fft.rfft(frame))  
+        spectrogram[i, :] = spectrum
+
+    return spectrogram
+
+def plot_spectrogram(spectrogram, sr, frame_size, hop_length):
+    spectrogram = 20 * np.log10(spectrogram + 1e-6)  # Convert to dB
+    time_axis = np.arange(spectrogram.shape[0]) * hop_length / sr
+    freq_axis = np.fft.fftfreq(frame_size, 1 / sr)[:frame_size // 2]
+
+    fig = go.Figure(data=go.Heatmap(
+        z=spectrogram.T,  
+        x=time_axis,
+        y=freq_axis,
+        # zmin=np.percentile(spectrogram, 0.5), 
+        # zmax=np.percentile(spectrogram, 99.5),
+        colorbar=dict(title="Amplituda (dB)"),
+    ))
+
+    fig.update_layout(
+        title="Spektrogram",
+        xaxis_title="Czas (s)",
+        yaxis_title="Częstotliwość (Hz)",
+        yaxis=dict(type='log', tickvals=[10, 100, 1000, 10000], ticktext=['10 Hz', '100 Hz', '1 kHz', '10 kHz']), 
+        template='plotly_white',
+        height=450
+    )
+
+    return fig
 
 def plot_feature(time_axis, feature_values, feature_name, mode):
     """
     Tworzy interaktywny wykres dla wartości cechy w czasie.
     """
-
     frame_numbers = np.arange(1, len(feature_values) + 1)
 
     fig = go.Figure()
@@ -152,54 +216,93 @@ def plot_feature(time_axis, feature_values, feature_name, mode):
         title_x=0.5,
         xaxis_title="Czas (s)",
         yaxis_title=f"{feature_name}",
-        hovermode="closest" 
+        hovermode="closest",
+        height=450, 
     )
 
     return fig
 
 
-def plot_ersb(time_axis, energy_ratios_list, mode):
+def plot_ersb(time_axis, energy_ratios_list, mode, sr):
+    f_max = sr / 2
+    subbands = [
+        (0, int(0.057 * f_max)),
+        (int(0.057 * f_max), int(0.156 * f_max)),
+        (int(0.156 * f_max), int(0.399 * f_max)),
+        (int(0.399 * f_max), int(f_max))
+    ]
+    band_labels = [f"{low}–{high} Hz" for (low, high) in subbands]
 
     ersb1 = [e[0] for e in energy_ratios_list]
     ersb2 = [e[1] for e in energy_ratios_list]
     ersb3 = [e[2] for e in energy_ratios_list]
+    ersb4 = [e[3] for e in energy_ratios_list]
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=time_axis,
-                            y=ersb1,
-                            mode='lines+markers',
-                            name='ERSB1 (0–630 Hz)',
-                            hovertemplate=
-                            f"<b>Numer {mode}:</b> %{{customdata[0]}}<br>" +
-                            "<b>Czas:</b> %{x:.3f} s" + "<br>" +
-                            f"<b>ERSB1 (0–630 Hz)'</b> %{{y:.3f}}" + "<extra></extra>"))
-    fig.add_trace(go.Scatter(x=time_axis,
-                            y=ersb2,
-                            mode='lines+markers',
-                            name='ERSB2 (630–1720 Hz)',
-                            hovertemplate=
-                            f"<b>Numer {mode}:</b> %{{customdata[0]}}<br>" +
-                            "<b>Czas:</b> %{x:.3f} s" + "<br>" +
-                            f"<b>ERSB2 (630–1720 Hz)'</b> %{{y:.3f}}" + "<extra></extra>"))
-    fig.add_trace(go.Scatter(x=time_axis,
-                            y=ersb3,
-                            mode='lines+markers',
-                            name='ERSB3 (1720–4400 Hz)',
-                            hovertemplate=
-                            f"<b>Numer {mode}:</b> %{{customdata[0]}}<br>" +
-                            "<b>Czas:</b> %{x:.3f} s" + "<br>" +
-                            f"<b>ERSB3 (1720–4400 Hz)'</b> %{{y:.3f}}" + "<extra></extra>"))
+
+    fig.add_trace(go.Scatter(
+        x=time_axis,
+        y=ersb1,
+        mode='lines',
+        name=f'ERSB1 ({band_labels[0]})',
+        line=dict(width=0.5, color='rgba(160, 197, 127, 0.8)'),
+        stackgroup='one',
+        groupnorm='fraction',
+        hovertemplate=f"<b>{mode}:</b> %{{x}} s<br>ERSB1: %{{y:.3f}}<extra></extra>"
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=time_axis,
+        y=ersb2,
+        mode='lines',
+        name=f'ERSB2 ({band_labels[1]})',
+        line=dict(width=0.5, color='rgba(236, 90, 83, 0.8)'),
+        stackgroup='one',
+        hovertemplate=f"<b>{mode}:</b> %{{x}} s<br>ERSB2: %{{y:.3f}}<extra></extra>"
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=time_axis,
+        y=ersb3,
+        mode='lines',
+        name=f'ERSB3 ({band_labels[2]})',
+        line=dict(width=0.5, color='rgba(245, 191, 79, 0.8)'),
+        stackgroup='one',
+        hovertemplate=f"<b>{mode}:</b> %{{x}} s<br>ERSB3: %{{y:.3f}}<extra></extra>"
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=time_axis,
+        y=ersb4,
+        mode='lines',
+        name=f'ERSB4 ({band_labels[3]})',
+        line=dict(width=0.5, color='rgba(43, 102, 194, 0.8)'),
+        stackgroup='one',
+        hovertemplate=f"<b>{mode}:</b> %{{x}} s<br>ERSB4: %{{y:.3f}}<extra></extra>"
+    ))
 
     fig.update_layout(
         title='ERSB (Energy Ratio Subbands)',
-        xaxis_title='Numer ramki',
+        xaxis_title="Czas (s)",
         yaxis_title='Proporcja energii',
         yaxis=dict(range=[0, 1]),
-        legend=dict(x=0.01, y=0.99),
         template='plotly_white',
-        height=400
+        legend=dict(x=0.01, y=0.99),
+        height=450
     )
 
+    return fig
+
+
+
+def plot_spectrum(frequencies, spectrum):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=frequencies, y=np.abs(spectrum), mode='lines', name='Widmo'))
+    fig.update_layout(
+        xaxis_title='Częstotliwość (Hz)',
+        yaxis_title='Amplituda',
+        hovermode="closest"
+    )
     return fig
 
 
